9dce77bc6bdd56bc1b733c57c0fe9fbe73cc094b
pedroffd <pedro.alcarin@gmail.com>
2026-02-17 19:54:34 -0300

    fix: implement scrollBehavior to handle navigation and hash anchors

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -50,6 +50,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, _from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return {
+        el: to.hash,
+        behavior: "smooth",
+      };
+    }
+    return { top: 0, behavior: "smooth" };
+  },
 });
 
 router.beforeEach((to, _from, next) => {
